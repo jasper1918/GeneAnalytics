ed79e7749c83436452fb069c61b20d49d4db6d16
jasper1918 <jasper1918@gmail.com>
2014-07-31 19:06:39 -0400

    R package install script

--- resources/scripts/Install_packages.R
+++ resources/scripts/Install_packages.R
@@ -1,18 +1,18 @@
 source("http://bioconductor.org/biocLite.R")
 
-if(!require(abind))   { biocLite("abind")}
-if(!require(corrplot))  { biocLite("corrplot")}
-if(!require(gage))   { biocLite("gage")}
-if(!require(genefu))   { biocLite("genefu")}
-if(!require(ggplot2))   { biocLite("ggplot2")}
-if(!require(gplots))   { biocLite("gplots")}
-if(!require(hgu133a.db))     { biocLite("hgu133a.db")  }
-if(!require(igraph))   { biocLite("igraph")}
-if(!require(RColorBrewer))      biocLite("RColorBrewer")
-if(!require(reshape))   { biocLite("reshape")}
-if(!require(rmeta))   { biocLite("rmeta")}
-if(!require(RSQLite))   { biocLite("RSQLite")}
-if(!require(survcomp))   { biocLite("survcomp")}
-if(!require(survival))   { biocLite("survival")}
+if(!require(abind))   { biocLite("abind",lib='/usr/local/lib/R/site-library')}
+if(!require(corrplot))  { biocLite("corrplot",lib='/usr/local/lib/R/site-library')}
+if(!require(gage))   { biocLite("gage",lib='/usr/local/lib/R/site-library')}
+if(!require(genefu))   { biocLite("genefu",lib='/usr/local/lib/R/site-library')}
+if(!require(ggplot2))   { biocLite("ggplot2",lib='/usr/local/lib/R/site-library')}
+if(!require(gplots))   { biocLite("gplots",lib='/usr/local/lib/R/site-library')}
+if(!require(hgu133a.db))     { biocLite("hgu133a.db",lib='/usr/local/lib/R/site-library') }
+if(!require(igraph))   { biocLite("igraph",lib='/usr/local/lib/R/site-library')}
+if(!require(RColorBrewer))      {biocLite("RColorBrewer",lib='/usr/local/lib/R/site-library')}
+if(!require(reshape))   { biocLite("reshape",lib='/usr/local/lib/R/site-library')}
+if(!require(rmeta))   { biocLite("rmeta",lib='/usr/local/lib/R/site-library')}
+if(!require(RSQLite))   { biocLite("RSQLite",lib='/usr/local/lib/R/site-library')}
+if(!require(survcomp))   { biocLite("survcomp",lib='/usr/local/lib/R/site-library')}
+if(!require(survival))   { biocLite("survival",lib='/usr/local/lib/R/site-library')}
 
 if(!require(jetset))   { cat("Install Jetset")}
